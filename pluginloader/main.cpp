e2e107510c26ee7d50d30c0a037c4f676ea491d7
Arkangel <arkangelhacket@gmail.com>
2012-04-04 22:34:36 +0200

    Agregada notificacion de descarga y liverado de secciones

--- pluginloader/main.cpp
+++ pluginloader/main.cpp
@@ -8,23 +8,26 @@ void DLL_EXPORT SomeFunction(const LPCSTR sometext)
 
 bool DLL_EXPORT WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-    SomeFunction("hola");
     switch (fdwReason)
     {
         case DLL_PROCESS_ATTACH:
             // attach to process
             // return FALSE to fail DLL load
+            SomeFunction("OnAttach");
             break;
 
         case DLL_PROCESS_DETACH:
+			SomeFunction("OnDetach");
             // detach from process
             break;
 
         case DLL_THREAD_ATTACH:
+			SomeFunction("OnNewThread");
             // attach to thread
             break;
 
         case DLL_THREAD_DETACH:
+			SomeFunction("OnExitThread");
             // detach from thread
             break;
     }
