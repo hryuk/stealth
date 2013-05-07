17e2bd062bbe2aba24e39135610b1a9f5df547fa
hzero <edusvh@gmail.com>
2013-05-07 06:39:49 +0200

    Multiconexión funcional (error relacionado con el pluginmanager)

--- cliente/gui/grouptreewidget.cpp
+++ cliente/gui/grouptreewidget.cpp
@@ -23,14 +23,10 @@ GroupTreeWidget::GroupTreeWidget(bool expanded,QWidget*)
     treewidget->setSelectionBehavior(QAbstractItemView::SelectRows);
     treewidget->setRootIsDecorated(false);
     treewidget->setHeaderHidden(true);
-    treewidget->setColumnCount(2);
+    treewidget->setColumnCount(1);
     treewidget->setIndentation(0);
     treewidget->setAnimated(true);
-    treewidget->setIconSize(QSize(40,40));
-    treewidget->header()->setStretchLastSection(false);
-    treewidget->header()->setResizeMode(0,QHeaderView::Stretch);
-    treewidget->header()->setResizeMode(1,QHeaderView::Fixed);
-    treewidget->setColumnWidth(1,200);
+    treewidget->header()->setStretchLastSection(true);
     treewidget->setFrameShape(QFrame::NoFrame);
     if(!expanded) treewidget->hide();
     frameLayout->addWidget(treewidget);
@@ -49,7 +45,6 @@ void GroupTreeWidget::addItem(Connection *connection)
 
     /* Creamos un nuevo item y seteamos el icono */
     QTreeWidgetItem* item=new QTreeWidgetItem();
-    item->setIcon(0,QIcon(":/res/img/blue-iconset/user.png"));
 
     /* Guardamos un ID único para el item usando el UserRole */
     item->setData(0,Qt::UserRole,QVariant(connection->getID()));
@@ -59,63 +54,5 @@ void GroupTreeWidget::addItem(Connection *connection)
 
     /* Creamos y añadimos al item los labels de información */
     ItemInfo* info=new ItemInfo(connection,this);
-    treewidget->setItemWidget(treewidget->topLevelItem(treewidget->topLevelItemCount()-1),0,info);
-
-    /* Creamos el GraphicsView y lo añadimos al item */
-    QGraphicsView* gvSpeed=new QGraphicsView();
-    gvSpeed->setRenderHints(QPainter::Antialiasing|QPainter::SmoothPixmapTransform);
-    gvSpeed->setFocusPolicy(Qt::NoFocus);
-    gvSpeed->setStyleSheet("border: 1px solid grey");
-    gvSpeed->setFixedHeight(50);
-    gvSpeed->setFixedWidth(200);
-    treewidget->setItemWidget(treewidget->topLevelItem(treewidget->topLevelItemCount()-1),1,gvSpeed);
-
-
-    /* Añadimos líneas aleatorias al GraphicsView como muestra */
-    QGraphicsScene* scene=new QGraphicsScene();
-    scene->setSceneRect(gvSpeed->rect());
-
-    QTime time = QTime::currentTime();
-    qsrand((uint)time.msec());
-
-    for(int i=0;i<3;i++)
-    {
-        QColor color;
-
-        switch(i)
-        {
-            case 0:
-                color=Qt::darkGreen;
-            break;
-            case 1:
-                color=Qt::darkRed;
-            break;
-            case 2:
-                color=Qt::darkBlue;
-            break;
-        }
-
-        QPen pen;
-        pen.setColor(color);
-        pen.setWidth(2);
-        pen.setCapStyle(Qt::RoundCap);
-        pen.setJoinStyle(Qt::RoundJoin);
-        int lastX=0,lastY=0;
-
-        for(int i=0;i<20;i++)
-        {
-            int newHeight=qrand()%48;
-            scene->addLine(lastX,lastY,lastX+10,newHeight,pen);
-            lastX+=10;
-            lastY=newHeight;
-        }
-    }
-
-    QGraphicsTextItem* textItem = new QGraphicsTextItem();
-    textItem->setPos(60,0);
-    textItem->setHtml("<font size=\"14\"><b>DEMO</b></font>");
-    scene->addItem(textItem);
-
-    gvSpeed->setScene(scene);
-    gvSpeed->fitInView(scene->sceneRect());
+    treewidget->setItemWidget(item,0,info);
 }
--- cliente/gui/iteminfo.cpp
+++ cliente/gui/iteminfo.cpp
@@ -12,6 +12,58 @@ ItemInfo::ItemInfo(Connection *connection, QWidget *parent) :
     {
         ui->label_4->setText(connection->peerAddress().toString());
     }
+
+    ui->gvSpeed->setRenderHints(QPainter::Antialiasing|QPainter::SmoothPixmapTransform);
+    ui->gvSpeed->setFocusPolicy(Qt::NoFocus);
+    ui->gvSpeed->setStyleSheet("border: 1px solid grey");
+
+    /* Añadimos líneas aleatorias al GraphicsView como muestra */
+    QGraphicsScene* scene=new QGraphicsScene();
+    scene->setSceneRect(ui->gvSpeed->rect());
+
+    QTime time = QTime::currentTime();
+    qsrand((uint)time.msec());
+
+    for(int i=0;i<3;i++)
+    {
+        QColor color;
+
+        switch(i)
+        {
+            case 0:
+                color=Qt::darkGreen;
+            break;
+            case 1:
+                color=Qt::darkRed;
+            break;
+            case 2:
+                color=Qt::darkBlue;
+            break;
+        }
+
+        QPen pen;
+        pen.setColor(color);
+        pen.setWidth(2);
+        pen.setCapStyle(Qt::RoundCap);
+        pen.setJoinStyle(Qt::RoundJoin);
+        int lastX=0,lastY=0;
+
+        for(int i=0;i<20;i++)
+        {
+            int newHeight=qrand()%48;
+            scene->addLine(lastX,lastY,lastX+10,newHeight,pen);
+            lastX+=10;
+            lastY=newHeight;
+        }
+    }
+
+    QGraphicsTextItem* textItem = new QGraphicsTextItem();
+    textItem->setPos(60,0);
+    textItem->setHtml("<font size=\"14\"><b>DEMO</b></font>");
+    scene->addItem(textItem);
+
+    ui->gvSpeed->setScene(scene);
+    ui->gvSpeed->fitInView(scene->sceneRect());
 }
 
 ItemInfo::~ItemInfo()
